093db3d919e421fe4611b5af6be9833631776b67
amirahmadadibi <amirahmadadibi@gmail.com>
2020-07-17 11:42:38 +0430

    compelete dependency injection process for applicaiton class

---
+++ app/src/main/java/com/mindorks/bootcamp/learndagger/di/components/ApplicationComponent.java
@@ -0,0 +1,26 @@
+package com.mindorks.bootcamp.learndagger.di.components;
+
+
+import com.mindorks.bootcamp.learndagger.MyApplication;
+import com.mindorks.bootcamp.learndagger.di.modules.ApplicationModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+
+//@Component is dependency manager - is in the middle between class and module
+//take module's that needs to provide instances
+//component searchs for module witch can provides the instances
+//Dagger will create a class with the name of this interface and adds Dagger in front of it
+//so crates class DaggerApplicationComponent and implement this interface on it
+
+@Singleton
+@Component(modules = {ApplicationModule.class})
+public interface ApplicationComponent {
+    //you can call it supply or inject
+    //by passing application to inject method of this interface
+    //we tell dagger hay MyApplication class needs dependencies
+    //scan that class
+    void inject(MyApplication application);
+}
